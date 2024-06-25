b007b4ec187bf10ba0d22ea338b8405e3ab068b4
induchabada <induchabada@gmail.com>
2024-06-25 14:15:39 -0400

    Time: 1545 ms (7.48%), Space: 16.4 MB (70%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        result = 0
+        for i in range(x+1):
+            
+            if i*i == x:
+                result = i
+                break
+            elif i*i > x:
+                result = i-1
+                break
+        return result
+        
