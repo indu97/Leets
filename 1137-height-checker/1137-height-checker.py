0bf4ffa093c8f181f83da7510ae648ac1e6ff8f5
induchabada <induchabada@gmail.com>
2024-06-26 14:51:28 -0400

    Time: 88 ms (6.19%), Space: 36 MB (13.21%) - LeetHub

--- 1137-height-checker/1137-height-checker.py
+++ 1137-height-checker/1137-height-checker.py
@@ -1,33 +1,5 @@
 import numpy as np
+from collections import Counter
 class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        # oldHeights = np.copy(heights)
-        # heights.sort()
-        # count = 0
-        # for i in range(len(oldHeights)):
-        #     if oldHeights[i] != heights[i]:
-        #         count += 1
-        # return count
-        
-        oldHeights = np.copy(heights)
-        n = len(heights)
-        count = 0
-        for i in range(n):
-            min = heights[i]
-            idx_min = i
-            for j in range(i+1,n):
-                if heights[j] < min:
-                    min = heights[j]
-                    idx_min = j
-            if min != heights[i]:  
-                #count += 1
-                temp = heights[i]
-                heights[i] = min
-                heights[idx_min] = temp
-                #if idx_min == n-1:
-                    #count += 1
-        for i in range(len(heights)):
-            if oldHeights[i] != heights[i]:
-                count += 1
-    
-        return count
+    def heightChecker(self, h: List[int]) -> int:
+        return sum(v != u for v,u in zip(h,sorted(h)))
