c878ed23e0a5c697eb09c9b235a6bd6d84e11e59
induchabada <induchabada@gmail.com>
2024-06-26 15:40:43 -0400

    Time: 45 ms (17.49%), Space: 16.5 MB (48.74%) - LeetHub

--- 1137-height-checker/1137-height-checker.py
+++ 1137-height-checker/1137-height-checker.py
@@ -1,5 +1,11 @@
-import numpy as np
-from collections import Counter
 class Solution:
-    def heightChecker(self, h: List[int]) -> int:
-        return sum(v != u for v,u in zip(h,sorted(h)))
+    def heightChecker(self, heights: List[int]) -> int:
+        l=heights.copy()
+        heights.sort()
+        ans=0
+        for i in range(len(l)):
+            if l[i]!=heights[i]:
+                ans+=1
+
+        return ans
+
