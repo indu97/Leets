0634c9e498bb1a44c22e45aaaac663c5be352ae3
induchabada <induchabada@gmail.com>
2024-07-09 16:21:25 -0400

    Time: 134 ms (42.67%), Space: 16.8 MB (83.95%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -6,12 +6,16 @@ class Solution:
                 if not prev and not val and not flowerbed[idx+1]:
                     n = n - 1
                     val = 1
-                prev = val
+                    prev = val
+                else: 
+                    prev = val
             else:
                 if not prev and not val:
                     n = n - 1
                     val = 1
-                prev = val
+                    prev = val
+                else: 
+                    prev = val
         if n <= 0:
             return True
 
