77fd19c4f7ebf2cf19d0297880d5e9c5f7ff2d39
induchabada <induchabada@gmail.com>
2024-08-27 12:16:08 -0400

    Time: 39 ms (85.71%), Space: 17.1 MB (54.82%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ''
+        for i in s.lower():
+            if i.isalnum():
+                new_s = new_s + i
+        n = len(new_s)
+        for i in range(n):
+            if new_s[i] != new_s[-i-1]:
+                print(i)
+                return False
+        return True
