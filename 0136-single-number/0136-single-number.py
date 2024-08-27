a33111f8343c5ae5ac3b62a13ddc5fad2a04db48
induchabada <induchabada@gmail.com>
2024-08-27 17:48:03 -0400

    Time: 110 ms (58.09%), Space: 19 MB (54.68%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        for key, count in c.items():
-            if count == 1:
-                return key
+        xor = 0
+        for i in nums:
+            xor = xor ^ i
+        return xor
