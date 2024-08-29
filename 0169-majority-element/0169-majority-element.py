d03d4f6b0fe010a348e8b49fa4510df8a46b11ce
induchabada <induchabada@gmail.com>
2024-08-28 22:16:18 -0400

    Time: 167 ms (72.32%), Space: 18 MB (21.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)/2
-        a = list(set(nums))
-        for i in a:
-            c = nums.count(i)
-            if c > n:
-                return i
+        # n = len(nums)/2
+        # a = list(set(nums))
+        # for i in a:
+        #     c = nums.count(i)
+        #     if c > n:
+        #         return i
+
+
+        # Boyer-Moore Majority Vote Algorithm:
+        votes = 0
+        candidate = nums[0]
+        for i in nums:
+            if votes == 0:
+                candidate = i
+            if i == candidate:
+                votes += 1
+            else:
+                votes -= 1
+        return candidate
