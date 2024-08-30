cccd53f3e4e03708bd91368dccb0d6e2c9dbb422
induchabada <induchabada@gmail.com>
2024-08-30 19:45:34 -0400

    Time: 56 ms (66.78%), Space: 17.8 MB (56.81%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        local_profit = 0
+        global_profit = 0
+        candidate = prices [0]
+        for price in prices:
+            if price < candidate:
+                # global_profit = global_profit + local_profit
+                local_profit = 0
+                candidate = price
+            else:
+                if local_profit > price - candidate:
+                    # global_profit += local_profit
+                    local_profit = 0
+                    candidate = price
+                else:
+                    global_profit -= local_profit
+                    local_profit = price - candidate
+                    global_profit += local_profit
+        return global_profit
