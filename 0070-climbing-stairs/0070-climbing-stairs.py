9daf3360fbdc053682075cc58292286bc5c46da4
induchabada <induchabada@gmail.com>
2024-09-05 13:03:40 -0400

    Time: 28 ms (91.2%), Space: 16.4 MB (90.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,14 @@
 class Solution:
+    def helper(self, i, dp) -> int:
+        if i == 1 or i == 2:
+            dp[i - 1] = i
+            return dp[i - 1]
+        elif dp[i - 1] != -1:
+            return dp[i - 1]
+        else:
+            dp[i - 1] = self.helper(i - 1, dp) + self.helper(i - 2, dp)
+            return dp[i - 1]
+
     def climbStairs(self, n: int) -> int:
-        # for i = 0 to n+1:
-        #     twos = n - i // 2
-        #     if twos = int(twos):
-        # if n == 1: 
-        #     return 1
-        # elif n == 2:
-        #     return 2
-        # else:
-        #     return self.climbStairs(n-1) + self.climbStairs(n-2)
-        arr = [0] * (n+1)
-        arr[1] = 1
-        if n >= 2:
-            arr[2] = 2
-            for i in range(3, n+1):
-                arr[i] = arr[i-1] + arr[i-2]
-        return arr[n]
+        dp = [-1] * (n)  
+        return self.helper(n, dp)
