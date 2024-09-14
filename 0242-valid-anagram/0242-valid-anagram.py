c0a358eb9e2437359dd1cec0784c700549fa93c6
induchabada <induchabada@gmail.com>
2024-09-14 13:52:42 -0400

    Time: 51 ms (46.37%), Space: 17.2 MB (25.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,7 +2,4 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s = ''.join(sorted(s))
         t = ''.join(sorted(t))
-        if s == t:
-            return True
-        else:
-            return False
+        return True if s == t else False
