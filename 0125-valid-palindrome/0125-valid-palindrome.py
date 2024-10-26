7f414583e54206d4941c3c7bb9b92461ca33cfd6
induchabada <induchabada@gmail.com>
2024-10-26 15:51:08 -0400

    Time: 7 ms (81.85%), Space: 18.3 MB (17.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,7 @@
+import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower().replace(" ", "")
-        p = ''
-        for c in s:
-            if c.isalnum():
-                p += c
-        return p == p[::-1]
+        s = re.sub(r'[^a-zA-Z0-9]+', '',s)
+        return s == s[::-1]
