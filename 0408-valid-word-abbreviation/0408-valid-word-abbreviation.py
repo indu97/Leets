5cb7d89274f765c251d574dd5520ae92d1600b99
induchabada <induchabada@gmail.com>
2024-10-28 15:00:24 -0400

    Time: 0 ms (100%), Space: 16.7 MB (23.11%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        m = len(word)
+        n = len(abbr)
+        word_i = 0
+        abbr_i = 0
+        while (word_i < m and abbr_i < n):
+            if abbr[abbr_i].isnumeric():
+                # If leading zero, return False
+                if abbr[abbr_i] == '0':
+                    return False
+                else:
+                    # If c is numeric, get the whole number
+                    num = ''
+                    for i in range(abbr_i, n):
+                        if abbr[i].isnumeric():
+                            num = num + abbr[i]
+                        else:
+                            break
+                        abbr_i += 1
+
+                    # Advance the word ptr by num indices
+                    word_i += int(num)
+
+                    # If word_i > end of word
+                    if word_i > m:
+                        return False
+            
+            # If c is an alphabet
+            elif abbr[abbr_i].isalpha():
+                if word[word_i] != abbr[abbr_i]:
+                    return False
+                else:
+                    word_i += 1
+                    abbr_i += 1
+
+        # If not end of word or abbr, return False
+        if not (word_i == m and abbr_i == n):
+            return False
+        else:
+            return True
+
