41d7b3dd58169afc2e8e477ad152207a43950275
induchabada <induchabada@gmail.com>
2024-10-27 21:50:52 -0400

    Time: 39 ms (67.95%), Space: 18.2 MB (58.71%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -1,7 +1,8 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
-        offensive_idx = []
+        s_list = list(s)
+        counter = 0
         for idx, c in enumerate(s):
             if c == '(':
                 stack.append([idx, c])
@@ -9,14 +10,41 @@ class Solution:
                 if stack:
                     stack.pop()
                 else:
-                    offensive_idx.append(idx)
+                    s_list.pop(idx - counter)
+                    counter += 1
+        
         while stack:
             popped_array = stack.pop()
             idx = popped_array[0]
-            offensive_idx.append(idx)
+            s_list.pop(idx - counter)
         
-        res = ''
-        for idx, c in enumerate(s):
-            if idx not in offensive_idx:
-                res = res + c
-        return res
+        return ''.join(s_list)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # stack = []
+        # offensive_idx = []
+        # for idx, c in enumerate(s):
+        #     if c == '(':
+        #         stack.append([idx, c])
+        #     elif c == ')':
+        #         if stack:
+        #             stack.pop()
+        #         else:
+        #             offensive_idx.append(idx)
+        # while stack:
+        #     popped_array = stack.pop()
+        #     idx = popped_array[0]
+        #     offensive_idx.append(idx)
+        
+        # res = ''
+        # for idx, c in enumerate(s):
+        #     if idx not in offensive_idx:
+        #         res = res + c
+        # return res
