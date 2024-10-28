7248abff4f6bb3f79a9b68ebf3cb66fe6f544af6
induchabada <induchabada@gmail.com>
2024-10-27 21:19:59 -0400

    Time: 56 ms (62.72%), Space: 17 MB (72.41%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,14 +1,14 @@
 class Solution:
-    def pal_check(self, s: str, start: int, end: int, counter: int) -> bool:
-        while start < end:
-            if s[start] != s[end]:
+    def pal_check(self, s: str, left, right, counter):
+        while(left < right):
+            if s[left] != s[right]:
                 if counter == 1:
                     return False
-                # Skip the left character or the right character
-                return self.pal_check(s, start + 1, end, 1) or self.pal_check(s, start, end - 1, 1)
-            start += 1
-            end -= 1
+                return self.pal_check(s, left + 1, right, 1) or self.pal_check(s, left, right - 1, 1)
+            left += 1
+            right -= 1
         return True
+            
 
     def validPalindrome(self, s: str) -> bool:
         return self.pal_check(s, 0, len(s) - 1, 0)
