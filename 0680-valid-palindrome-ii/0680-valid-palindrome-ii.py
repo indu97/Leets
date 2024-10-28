874a4469fcca6e092b8647c0bac3072e536e0a37
induchabada <induchabada@gmail.com>
2024-10-27 21:07:26 -0400

    Time: 99 ms (8.08%), Space: 16.8 MB (97.99%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,17 +1,19 @@
+import math
+
 class Solution:
-    def pal_check(self, s: str, counter):
-        n = len(s)
-        for i in range(n // 2):
+    def pal_check(self, s: str, start, end, counter):
+        print(s[start:end+1])
+        n = end - start + 1
+        for i in range(n):
             # if forward and backward pointer are not same, del once
-            if s[i] != s[n - i - 1]:
+            # print(s[i], s[end - i - 1])
+            if s[start + i] != s[end - i]:
                 if counter == 0:
-                    s_1 = s[:i] + s[i + 1:]
-                    s_2 = s[:n - i - 1] + s[n - i:]
-                    return self.pal_check(s_1, 1) or self.pal_check(s_2, 1)
+                    return self.pal_check(s, i + 1, n - i - 1, 1) or self.pal_check(s, i, n - i - 2, 1)
                 else:
                     return False
         return True
             
 
     def validPalindrome(self, s: str) -> bool:
-        return self.pal_check(s, 0)
+        return self.pal_check(s, 0, len(s) - 1, 0)
