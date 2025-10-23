095976a785691c9488f5efe13c9b425f4d54fa3b
induchabada <induchabada@gmail.com>
2025-10-23 19:12:55 -0400

    Time: 39 ms (48.66%), Space: 17.6 MB (94.28%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -3,14 +3,10 @@ class Solution:
         m = len(word1)
         n = len(word2)
         out = ""
-        for i in range(m+n):
-            if (i < m and i < n):
+        for i in range(max(m,n)):
+            if (i < m):
                 out += word1[i]
+            if (i < n):
                 out += word2[i]
-            else:
-                if (i < m):
-                    out += word1[i]
-                if (i < n):
-                    out += word2[i]
         return out
         
