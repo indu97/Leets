014fc17e4702ba2687049ebb2beab64345912d18
induchabada <induchabada@gmail.com>
2025-10-26 17:36:05 -0400

    Time: 0 ms (100%), Space: 17.7 MB (71.09%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,26 +1,4 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
-        if len(str1) < len(str2):
-            small_str = str1
-            big_str = str2
-        else: 
-            small_str = str2    
-            big_str = str1
-        
-        m = len(small_str)
-        n = len(big_str)
-
-        key = small_str
-        for j in range(m,0, -1):
-            key = small_str[:j]
-            for i in range(0,n,j):       
-                if big_str[i:i+j]!= key:
-                    break
-            else:  # inner loop didn't break
-                if small_str == key * (m // j):
-                    return key
-        return ""
-                
-
-        
+        return str1[:gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
