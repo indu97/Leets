c2b82d60e0020632d402b045aca44ced0e8b4277
induchabada <induchabada@gmail.com>
2025-11-01 19:56:04 -0400

    Time: 7 ms (79.99%), Space: 19.9 MB (11.5%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -2,40 +2,18 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
         out = [0] * n
-        if nums[0] >= 0:
-            for i in range(n):
-                out[i] = nums[i] * nums[i]
-            return out
-        
-        m = n
-        for i, x in enumerate(nums):
-            if x >= 0:
-                m = i
-                break
-        if m == n:
-            for i in range(n):
-                out[i] = nums[n-i-1] * nums[n-i-1]
-            return out
 
-        i, j, k = m-1, m, 0
-        while(i >= 0 and j <= n-1):
-            if abs(nums[i]) < nums[j]:
-                out[k] = nums[i] * nums[i]
-                i -= 1
+        l, r, i = 0, n-1, n-1
+        while l <= r:
+            left = abs(nums[l])
+            right = nums[r]
+            if left > right:
+                out[i] = left * left
+                l += 1
             else:
-                out[k] = nums[j] * nums[j]
-                j += 1
-            k += 1
-        
-        while i >= 0:
-            out[k] = nums[i] * nums[i]
+                out[i] = right * right
+                r -= 1
             i -= 1
-            k += 1
-        
-        while j <= n-1:
-            out[k] = nums[j] * nums[j]
-            j += 1
-            k += 1
         return out
 
 
