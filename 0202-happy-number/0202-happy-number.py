144790c0f708afd6da9bfc9d81e0d3d1c3faa348
induchabada <induchabada@gmail.com>
2025-11-16 17:46:54 -0500

    Time: 3 ms (41.4%), Space: 18 MB (17.49%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,22 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        
+        def sumOfSquares(n: int) -> int:
+            sum = 0
+            while(n > 0):
+                r = n % 10
+                n = n // 10
+                sum = sum + r*r
+            return sum
+
         seen = set()
-        return self.isLooped(n, seen)
-
-    def isLooped(self, n: int, seen) -> bool:
-        s = self.sumOfSquares(n)
-        if s == 1:
-            return True
-        if s in seen:
-            return False
-        else:
+        s = sumOfSquares(n)
+        while(s not in seen):
+            if s == 1:
+                return True
             seen.add(s)
-            return self.isLooped(s, seen)
-
-    def sumOfSquares(self, n: int) -> int:
-        sum = 0
-        while(n > 0):
-            r = n % 10
-            n = n // 10
-            sum = sum + r*r
-        return sum 
+            s = sumOfSquares(s)
+        return False
