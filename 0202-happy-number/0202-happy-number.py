3900ef3324b20eb09628d78a5ed0b1728d1a3ce6
induchabada <induchabada@gmail.com>
2025-11-16 17:47:44 -0500

    Time: 3 ms (41.4%), Space: 17.7 MB (86.83%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,12 +2,12 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         
         def sumOfSquares(n: int) -> int:
-            sum = 0
+            total = 0
             while(n > 0):
                 r = n % 10
                 n = n // 10
-                sum = sum + r*r
-            return sum
+                total += r*r
+            return total
 
         seen = set()
         s = sumOfSquares(n)
